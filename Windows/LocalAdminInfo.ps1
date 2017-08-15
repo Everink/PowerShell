53504b1a7ff223016c5180bbd5e92aab9b4f348e
Everink <roeleverink@hotmail.com>
2017-08-15 08:03:42 +0200

    Update LocalAdminInfo.ps1

--- Windows/LocalAdminInfo.ps1
+++ Windows/LocalAdminInfo.ps1
@@ -1,10 +1,15 @@
 
+#Simple workflow to query a list of servers for the local administrator account. It is identified by using the SID, so it also works if the default name has changed.
+#Using a workflow here so we can utilise the "foreach -parallel", this significantly speeds up the process to query a long list of servers.
+#We use WMI to query, which uses RPC, and if that fails we try the Get-CimInstance method because this uses the WinRM method.
+#If both methods fail, we put the name of the server in an array, and in the end store it in a file
+ 
  
 workflow Get-Admininfo 
 {    
     Param
     (
-        # Serverlist to query for local administrator
+        # Serverlist to query for local administrator account
         [Parameter(Mandatory = $true)][string[]]$Servers,
 
         # If there are offline servers, or servers that can't be reached, this is the path to the txt file that contains those.
